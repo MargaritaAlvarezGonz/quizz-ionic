7974b3482a7f0c2b2b69b2171571e9ba9153419a
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-08-25 05:30:36 -0600

    added api service

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
 
 @Component({
   selector: 'app-game',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePage implements OnInit {
 
-  constructor() { }
+  questions: any = [];
+  questiontitle: any;
+  questionAnswer: any;
+  heplAnswer: any;
 
+  //inicializamos en 0 las respuestas, pero necesitamos que vayan cambiando, por lo que vamos a hacer una variable que se llama questionNumber
+
+  questionNumber: any;
+
+
+   //El serviceApi se tiene que inicializar en el constructor y se importa
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+    //aquí vamos a llamar a las preguntas cuando inicie la aplicacion
   ngOnInit() {
+    this.getQuestions();
+    this.questionNumber = 0;
+
+  }
+
+
+  // Esta es la función que va a llamar a la Api, que a su vez, ya solicitó los datos al archivo json
+
+  getQuestions(){
+    this.apiService.getQuestions().subscribe(
+      (res) => {
+        this.questions = res; //nos trae
+        this.questiontitle = this.questions[this.questionNumber].question; //al titulo de las preguntas en nuestro json se llaman question
+        this.questionAnswer = this.questions[this.questionNumber].answer;
+        this.heplAnswer = this.questions[this.questionNumber].help;
+      }, (err) => {
+        console.log("error: ", err)
+      }
+    )
   }
 
 }
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  //hay que inicializar la petición de http client
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getQuestions(){
+    return this.http.get('../../assets/files/questions.json').pipe(
+      map((res: any) => {
+        return res.data;
+      }) // Con el mapeo nos trae directamente los datos que estan dentro del json
+    );
+  }
+  // luego, vamos a hacer la petición a este servicio, el de getQuestions que creamos
+  //y se hace desde la pagina del juego para obtener todas las preguntas
+
+}
+
